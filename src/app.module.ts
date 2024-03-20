77179a9aac9b9731a3ddcd645eea362b5fa1e7bc
Mickael Zana <zanamickael@gmail.com>
2024-03-20 17:27:30 +0200

    Basic mongo setup and config env var

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+  ],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })
