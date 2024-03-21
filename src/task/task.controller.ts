5ef884e8df4bf9ba2994e399e4732e51b879d132
Mickael Zana <zanamickael@gmail.com>
2024-03-21 14:41:13 +0200

    Implement CRUD for tasks

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,15 +1,42 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dtos/create-task.dto';
 import { Task } from './interfaces';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('task')
 export class TaskController {
   constructor(private taskService: TaskService) {}
+
+  @Get()
+  async getTasks(@Request() req): Promise<Task[]> {
+    const { id } = req.user;
+    return this.taskService.getTasks(id);
+  }
 
   @Post('create')
   async createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     console.log(createTaskDto);
     return this.taskService.createTask(createTaskDto);
+  }
+
+  @Post('update')
+  async updateTask(@Body() updateTaskDto: CreateTaskDto): Promise<Task> {
+    console.log(updateTaskDto);
+    return this.taskService.updateTask(updateTaskDto);
+  }
+
+  @Post('delete/:id')
+  async deleteTask(@Param('id') id: string): Promise<Task> {
+    return this.taskService.deleteTask(id);
   }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { CreateTaskDto } from './dtos/create-task.dto';
 import { TaskModel } from './task.schema';
 import { Task } from './interfaces';
@@ -9,9 +9,24 @@ import { Task } from './interfaces';
 export class TaskService {
   constructor(@InjectModel('Task') private taskModel: Model<TaskModel>) {}
 
+  async getTasks(user: Types.ObjectId): Promise<Task[]> {
+    return this.taskModel.find({ user }).exec();
+  }
+
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
     const { content, status, user } = createTaskDto;
     const task = new this.taskModel({ content, status, user });
     return task.save();
+  }
+
+  async updateTask(updateTaskDto: CreateTaskDto): Promise<Task> {
+    const { content, status, user } = updateTaskDto;
+    const task = await this.taskModel.findOne({ content, user });
+    task.status = status;
+    return task.save();
+  }
+
+  async deleteTask(id: string): Promise<Task> {
+    return this.taskModel.findOneAndDelete({ _id: id });
   }
 }
